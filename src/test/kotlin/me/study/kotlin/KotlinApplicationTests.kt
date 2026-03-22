c6cb3a5f639add9b930e1881b131f2fb21b729a2
Jeongsu Yang <mycat83@gmail.com>
2026-03-22 12:36:33 +0900

    chore: 초기 Spring Boot + Kotlin 프로젝트 설정 추가

---
+++ src/test/kotlin/me/study/kotlin/KotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.study.kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
