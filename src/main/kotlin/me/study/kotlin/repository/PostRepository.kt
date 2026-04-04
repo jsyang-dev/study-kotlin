ad849e405586099dae1c670a8f837c8c39d251b0
Jeongsu Yang <mycat83@gmail.com>
2026-04-04 22:08:13 +0900

    refactor: PostRepository 패키지 위치 변경

--- src/main/kotlin/me/study/kotlin/repository/PostRepository.kt
+++ src/main/kotlin/me/study/kotlin/repository/PostRepository.kt
@@ -1,5 +1,6 @@
-package me.study.kotlin.domain
+package me.study.kotlin.repository
 
+import me.study.kotlin.domain.Post
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface PostRepository : JpaRepository<Post, Long> {
